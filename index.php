4667c5fd097e3837d35a8bcb9d819532bd9184b0
Victorhck <victorhck@mailbox.org>
2022-04-22 21:41:29 +0200

    trad de index.php

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ require('info.php');
 
 	<div id="sidebar">
 
-		<div class="title">services</div>
+		<div class="title">servicios</div>
 		<ul id="services">
 <?php
 
@@ -39,7 +39,7 @@ if ($servers_list)
 
 ?>
 
-		<div class="title">servers</div>
+		<div class="title">servidores</div>
 		<ul id="servers">
 <?php
 
@@ -67,33 +67,33 @@ if ($servers_list)
 		<div class="title">uptime</div>
 		<div id="uptime" class="box">
 			<div>
-				<span id="uptime-days" class="value"><?php echo $uptime_days; ?></span> days, 
-				<span id="uptime-hours" class="value"><?php echo $uptime_hours; ?></span> hours, 
-				<span id="uptime-minutes" class="value"><?php echo $uptime_minutes; ?></span> minutes and
-				<span id="uptime-seconds" class="value"><?php echo $uptime_seconds; ?></span> seconds
+				<span id="uptime-days" class="value"><?php echo $uptime_days; ?></span> días, 
+				<span id="uptime-hours" class="value"><?php echo $uptime_hours; ?></span> horas, 
+				<span id="uptime-minutes" class="value"><?php echo $uptime_minutes; ?></span> minutos y
+				<span id="uptime-seconds" class="value"><?php echo $uptime_seconds; ?></span> segundos
 			</div>
 		</div>
 
-		<div class="title">cpu load</div>
+		<div class="title">carga de cpu</div>
 		<div id="cpu" class="box">
 			<ul>
-				<li><span id="cpuload-1" class="value"><?php echo $cpu['1']; ?></span> (average over the last 1 minute)</li>
-				<li><span id="cpuload-5" class="value"><?php echo $cpu['5']; ?></span> (average over the last 5 minutes)</li>
-				<li><span id="cpuload-15" class="value"><?php echo $cpu['15']; ?></span> (average over the last 15 minutes)</li>
+				<li><span id="cpuload-1" class="value"><?php echo $cpu['1']; ?></span> (media del último 1 minuto)</li>
+				<li><span id="cpuload-5" class="value"><?php echo $cpu['5']; ?></span> (media de los últimos 5 minutos)</li>
+				<li><span id="cpuload-15" class="value"><?php echo $cpu['15']; ?></span> (media de los últimos 15 minutos)</li>
 			</ul>
 		</div>
 
-		<div class="title">memory usage</div>
+		<div class="title">uso de memoria</div>
 		<div id="meminfo" class="box">
 			<div>
 				<span id="mem-usage" class="value"><?php echo ($mem['total'] - $mem['free']); ?></span> /
 				<span id="mem-total"><?php echo $mem['total']; ?></span> <?php echo $mem_multiple; ?>
-				(<span id="mem-cached" class="value"><?php echo $mem['cached']; ?></span> <?php echo $mem_multiple; ?> cached)
+				(<span id="mem-cached" class="value"><?php echo $mem['cached']; ?></span> <?php echo $mem_multiple; ?> en caché)
 				<progress id="mem" value="<?php echo ($mem['total'] - $mem['free']); ?>" max="<?php echo $mem['total']; ?>"></progress>
 			</div>
 		</div>
 
-		<div class="title">disk space</div>
+		<div class="title">espacio del disco</div>
 		<div id="diskinfo" class="box">
 			<div>
 				<span id="disk-usage" class="value"><?php echo ($disk['total'] - $disk['free']); ?></span> /
@@ -105,16 +105,16 @@ if ($servers_list)
 		<div class="title">software</div>
 		<div id="software" class="box">
 			<ul>
-				<li>Server Hostname: <span class="value"><?php echo gethostname(); ?></span></li>
-				<li>Server IP: <span class="value"><?php echo $_SERVER['SERVER_ADDR']; ?></span></li>
-				<li>Operating System: <span class="value"><?php echo $distro; ?></span></li>
-				<li>Webserver: <span class="value"><?php echo $webserver; ?></span></li>
+				<li>Hostname del servidor: <span class="value"><?php echo gethostname(); ?></span></li>
+				<li>IP del servidor: <span class="value"><?php echo $_SERVER['SERVER_ADDR']; ?></span></li>
+				<li>Sistema operativo: <span class="value"><?php echo $distro; ?></span></li>
+				<li>Servidor web: <span class="value"><?php echo $webserver; ?></span></li>
 				<li>CPU: <span class="value"><?php echo $cpu_model; ?></span></li>
 			</ul>
 		</div>
 
 		<div id="footer">
-			page generated in <?php echo (microtime(true) - $start); ?> seconds // <a href="http://github.com/lfiore/status/">status script by lfiore</a>
+			página generada en <?php echo (microtime(true) - $start); ?> segundos // <a href="http://github.com/lfiore/status/">status es un script de lfiore</a>
 		</div>
 
 	</div>
